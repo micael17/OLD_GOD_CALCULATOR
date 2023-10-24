18a0fb25ba47d73414bd75bc914457220d884053
JIHONG KIM <micael17@naver.com>
2023-10-24 22:49:07 +0900

    연봉계산기 재커밋

--- assets/domain/etc/xlsx.ts
+++ assets/domain/etc/xlsx.ts
@@ -1,6 +1,4 @@
-import * as XLSX from 'xlsx';
 import type { incomeTax } from "Etc";
-
 
 /**
  * 입력한 월 수령액과 부양가족, 자녀수를 받아
@@ -14,7 +12,55 @@ import type { incomeTax } from "Etc";
 
     const deductionCnt = familyCnt + childCnt
 
-    await fetch('/api/xlsx')
+    fetch('/2023.xlsx')
+        .then((response) => response.arrayBuffer())
+        .then(async (arrayBuffer) => {
+            // @ts-ignore
+            const XLSX = await import("https://cdn.sheetjs.com/xlsx-0.20.0/package/xlsx.mjs");
+            const workbook = XLSX.read(arrayBuffer, { type: 'array' });
+            const sheetName = workbook.SheetNames[0]; // 시트 선택
+            const sheet = workbook.Sheets[sheetName];
+            const Acolumns: number[] = []
+            const Bcolumns: number[] = []
+
+            for (const key in sheet) {
+                if (key.includes('A') && typeof sheet[key].v === "number") { // A열 값을 받아온다.
+                    Acolumns.push(sheet[key].v * 1000);
+                }
+
+                if (key.includes('B') && typeof sheet[key].v === "number") { // B열 값을 받아온다.
+                    Bcolumns.push(sheet[key].v * 1000);
+                }
+            }
+
+            if (Acolumns.length !== Bcolumns.length) {
+                throw new Error('조견표의 이상/이하 값의 갯수가 다릅니다. 데이터 확인 필요.')
+            }
+
+            let idx: number | null = null
+            for (let i = 0; i < Acolumns.length; i++) {
+                if (income >= Acolumns[i] && income < Bcolumns[i]) {
+                    idx = i;
+                }
+            }
+
+            if(idx) {
+                const alphabet = mapNumberToAlphabet(deductionCnt < 1 ? 0 : deductionCnt - 1)
+                result.tax = sheet[alphabet + (idx + 2)].v // 첫번째 배열은 제목이니 버리고, idx는 0부터 시작하니 +1 더 추가
+                result.localTax = result.tax * 0.1
+                result.totalTax = result.tax + result.localTax
+            }
+        })
+        .catch(error => {
+            console.error('파일 다운로드 오류:', error);
+        })
+
+
+    // const test = XLSX.read('http://localhost:3000/2023.xlsx')
+    // console.log(test)
+    // XLSX.readFile('http://localhost:3000/2023.xlsx')
+
+    /*await fetch('/api/xlsx')
         .then(response => response.arrayBuffer())
         .then(data => {
             // XLSX 파일을 파싱하여 JSON 데이터로 변환
@@ -54,7 +100,7 @@ import type { incomeTax } from "Etc";
         })
         .catch(error => {
             console.error('파일 다운로드 오류:', error);
-        });
+        });*/
 
     return result
 }
--- server/xlsx.ts
+++ server/xlsx.ts
@@ -1,5 +1,6 @@
+/*
 import fs from "fs";
 import * as XLSX from 'xlsx';
 export default defineEventHandler((event) => {
     return XLSX.readFile(process.cwd() + '/public/2023.xlsx')
-})
+})*/
