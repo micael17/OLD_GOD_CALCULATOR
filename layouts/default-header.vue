df05d32bc5d4f7e49c0e19438f5cb2cf5e58087a
micael17@naver.com <comman17!@>
2023-11-07 16:57:44 +0900

    메뉴 반응형으로 변경

--- layouts/default-header.vue
+++ layouts/default-header.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const drawerRight = ref(false)
 </script>
 
 <template>
@@ -10,62 +11,69 @@
             <q-icon name="calculate" size="xl" style="padding-bottom: 0.1rem;"/>만능계산기
           </q-item>
         </q-toolbar-title>
-        <q-btn-dropdown stretch flat label="메뉴">
-          <q-list>
-            <q-item clickable v-close-popup tabindex="0" to="/">
-              <q-item-section>
-                <q-item-label header class="item-header">
-                  <q-icon name="calculate" size="sm"/>
-                  <div class="header-label"> 만능계산기</div>
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable v-close-popup tabindex="0" to="/tax">
-              <q-item-section>
-                <q-item-label>부가세 계산기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="1" to="/character">
-              <q-item-section>
-                <q-item-label>글자수세기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="2" to="/monthlySalary">
-              <q-item-section>
-                <q-item-label>월급 실수령액 계산기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="2" to="/annualSalary">
-              <q-item-section>
-                <q-item-label>연봉 실수령액 계산기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="3" to="/manAge">
-              <q-item-section>
-                <q-item-label>만나이 계산기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="4" to="/krEnAddr">
-              <q-item-section>
-                <q-item-label>한글-영문 주소변환기</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="99" to="/nh">
-              <q-item-section>
-                <q-item-label>Nh 계산기</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
 
+        <!-- PC 메뉴 -->
+        <q-btn-group class="desktop_menu" flat>
+          <q-btn flat padding="xs sm" to="/tax" label="부가세 계산기" />
+          <q-btn flat padding="xs sm" to="/monthlySalary" label="월급 계산기" />
+          <q-btn flat padding="xs sm" to="/annualSalary" label="연봉 계산기" />
+          <q-btn flat padding="xs sm" to="/character" label="글자수세기" />
+          <q-btn flat padding="xs sm" to="/manAge" label="만나이 계산기" />
+          <q-btn flat padding="xs sm" to="/krEnAddr" label="영문주소 변환" />
+        </q-btn-group>
+
+        <!-- 모바일 메뉴 -->
+        <q-btn class="mobile_menu" flat @click="drawerRight = !drawerRight" round dense icon="menu" />
+        <q-drawer
+            class="mobile_menu bg-grey-9"
+            side="right"
+            v-model="drawerRight"
+            overlay
+            :width="200"
+            :breakpoint="500"
+        >
+          <q-scroll-area class="fit">
+            <q-list>
+              <q-item clickable v-close-popup tabindex="0" to="/tax">
+                <q-item-section>
+                  <q-item-label>부가세 계산기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="1" to="/character">
+                <q-item-section>
+                  <q-item-label>글자수세기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="2" to="/monthlySalary">
+                <q-item-section>
+                  <q-item-label>월급 실수령액 계산기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="2" to="/annualSalary">
+                <q-item-section>
+                  <q-item-label>연봉 실수령액 계산기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="3" to="/manAge">
+                <q-item-section>
+                  <q-item-label>만나이 계산기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="4" to="/krEnAddr">
+                <q-item-section>
+                  <q-item-label>한글-영문 주소변환기</q-item-label>
+                </q-item-section>
+              </q-item>
+              <q-item clickable v-close-popup tabindex="99" to="/nh">
+                <q-item-section>
+                  <q-item-label>Nh 계산기</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-scroll-area>
+        </q-drawer>
       </q-toolbar>
     </q-header>
-<!--    <div v-if="showMenuList" id="dropdown-menu">
-      <a href="#">메뉴 항목 1</a>
-      <a href="#">메뉴 항목 2</a>
-      <a href="#">메뉴 항목 3</a>
-    </div>-->
   </div>
 </template>
 
@@ -79,15 +87,25 @@
   font-weight: bold;
 }
 
-.item-header {
-  padding: 0;
+/* 모바일 화면 감지 */
+@media only screen and (max-width: 949px) {
+  .mobile_menu {
+    display: inherit;
+  }
+
+  .desktop_menu {
+    display: none;
+  }
 }
 
-.header-label {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  vertical-align: middle;
-}
+/* 데스크톱 화면 감지 */
+@media only screen and (min-width: 950px) {
+  .mobile_menu {
+    display: none;
+  }
 
+  .desktop_menu {
+    display: inherit;
+  }
+}
 </style>
