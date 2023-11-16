5fce62dc07e95ab1307a3de9608d238ead0d9f77
jihongkim <jihongkim@smilegate.com>
2023-11-16 14:57:38 +0900

    SEO 최적, sitemap 업데이트

--- pages/ip.vue
+++ pages/ip.vue
@@ -1,4 +1,18 @@
 <script setup lang="ts">
+useHead({
+  title: '내 ip 주소 확인 - 만능계산기',
+  meta: [
+    {name: 'author', content: 'JHK'},
+    {name: 'keywords', content: '내 ip 주소 확인, ip 주소 확인, ip 주소, 공인 ip, 퍼블릭 ip, public ip, ip 확인, 주소'},
+    {name: 'description', content: '내 ip 주소를 빠르고 간편하게 확인해보세요. 보통 ip 주소는 공인 ip 또는 퍼블릭 ip를 의미합니다. 내 ip 주소 확인 - 만능계산기에서 확인하세요.'},
+    {name: 'og:locale', content: 'ko_KR'},
+    {name: 'og:type', content: 'website'},
+    {name: 'og:title', content: '내 ip 주소 확인 - 만능계산기'},
+    {name: 'og:description', content: '내 ip 주소를 빠르고 간편하게 확인해보세요. 보통 ip 주소는 공인 ip 또는 퍼블릭 ip를 의미합니다. 내 ip 주소 확인 - 만능계산기에서 확인하세요.'},
+    {name: 'og:url', content: 'https://god-caclulator.com/ip'},
+    {name: 'og:site_name', content: '만능 계산기'},
+  ],
+})
 
 const ip = ref('')
 
