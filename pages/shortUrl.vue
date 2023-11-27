d502ab14fff4c5f2e4e1f4de78551a4a2f702baa
jihongkim <jihongkim@smilegate.com>
2023-11-27 17:16:31 +0900

    단축 URL 기능 개선

--- pages/shortUrl.vue
+++ pages/shortUrl.vue
@@ -18,6 +18,47 @@ const originUrl = ref('')
 const shortUrl = ref('')
 const loading = ref(false)
 
+// 클릭 횟수를 쿠키에서 가져오는 함수
+function getClickCountFromCookie() {
+  const clickCountCookie = document.cookie
+      .split(';')
+      .find(cookie => cookie.trim().startsWith('clickCount='));
+
+  if (clickCountCookie) {
+    return parseInt(clickCountCookie.split('=')[1]);
+  }
+
+  return 0;
+}
+
+// 클릭 횟수를 쿠키에 설정하는 함수 (유효기간 5분)
+function setClickCountToCookie(count) {
+  const now = new Date();
+  now.setTime(now.getTime() + 5 * 60 * 1000); // 현재 시간에서 5분 후의 시간 설정
+  document.cookie = `clickCount=${count}; expires=${now.toUTCString()}`;
+}
+
+
+function buttonClickHandler() {
+  const clickCount = getClickCountFromCookie();
+
+  if (clickCount >= 10) {
+    alert('10회 이상 클릭하셨습니다. 5분 후에 다시 시도해주세요.');
+    return; // 클릭 횟수가 10회 이상이면 더 이상 처리하지 않습니다.
+  }
+
+  getShortUrl()
+
+  // 클릭 횟수 증가 및 쿠키에 저장
+  const updatedClickCount = clickCount + 1;
+  setClickCountToCookie(updatedClickCount);
+
+  // 클릭 횟수가 10회가 아니면서 마지막 클릭일 때 API 호출
+  if (updatedClickCount === 10) {
+    setTimeout(callAPI, 5000); // 5초 뒤에 API 호출
+  }
+}
+
 function validateUrl(str) {
   const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
@@ -41,17 +82,17 @@ const getShortUrl = () => {
     return;
   }
 
-  fetch('https://cors-anywhere-jihongkim.koyeb.app/https://openapi.naver.com/v1/util/shorturl', {
-    method: 'POST',
+  const myServer = 'https://port-0-cors-5mk12alpglyfww.sel5.cloudtype.app'
+  const api = 'https://openapi.naver.com/v1/util/shorturl'
+
+  fetch(`${myServer}/${api}?url=${originUrl.value}`, {
+    method: 'GET',
     headers: {
       'origin': 'https://god-caclulator.com',
       'Content-Type': 'application/json',
       'X-Naver-Client-Id': 'zGSqm9ZT8a6IngXswpbt',
       'X-Naver-Client-Secret': 'vo3G6oeWK8'
-    },
-    body: JSON.stringify({
-      url: originUrl.value
-    })
+    }
   })
   .then(response => response.json())
   .then(data => {
@@ -71,6 +112,7 @@ const getShortUrl = () => {
           <h4>단축 URL 만들기</h4>
           <hr>
           <ui-input
+              title=""
             v-model="originUrl"
             label="원본 URL"
             hint="변환하려는 원본 URL을 입력하세요, ex) http://my-site.com"
